791cb657d9ffb33677cee9419dfc090c0640438a
hasanasiabd <hasanasiabd12@gmail.com>
2026-08-20 03:08:57 +0600

    Update firebase config to use environment variables

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,5 @@
+// client/src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { 
   getAuth, 
@@ -8,13 +10,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwba15VJpqEXlzOZ6egKAdmv2oFiQ-0z4",
-  authDomain: "taskmaster-app-5af3d.firebaseapp.com",
-  projectId: "taskmaster-app-5af3d",
-  storageBucket: "taskmaster-app-5af3d.firebasestorage.app",
-  messagingSenderId: "158771660704",
-  appId: "1:158771660704:web:681946e8b558c1a8a146c5",
-  measurementId: "G-G7TB2Q4HEM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);

719064b958d840188000acff6fb68d0714e5bbfb
hasanasiabd <hasanasiabd12@gmail.com>
2026-08-20 13:06:43 +0600

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,16 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-});
-
-// Attach Token to every request automatically
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  withCredentials: true
 });
 
 export default API;
